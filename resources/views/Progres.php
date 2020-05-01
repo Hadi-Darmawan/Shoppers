9520adac0a20fb9bae95c220310d5824825bb45c
Hadi-Darmawan <ghad.darmawan@gmail.com>
2020-05-01 14:40:54 +0800

    Membuat daftar progres yang harus dikerjakan pada Modul 1 dan 2

---
+++ resources/views/Progres.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Progress Praktikum</title>
+</head>
+<body>
+    
+    Progres Modul 1
+        1. Membuat Validasi Input Registrasi Admin
+        2. Membuat Validasi Input Registrasi User
+        3. Membuat Validasi Input Login Admin
+        4. Membuat Validasi Input Login User
+
+    Progres Modul 2
+        1. Membuat CRUD Courier
+        2. Membuat CRUD Product
+        2. Membuat CRUD Detail_Kategori
+
+</body>
+</html>
